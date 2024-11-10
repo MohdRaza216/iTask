e4d74b1396b6c78b082a34f16a3bbe93966bcb1c
MohdRaza216 <mohdrazamoghul2024@gmail.com>
2024-11-10 12:15:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+      <div className="container">
+        <div className="bg-red-600">
+          Hello I am red!
+        </div>
+      </div>
+  )
+}
+
+export default App
